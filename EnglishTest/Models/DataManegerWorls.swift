460560e1d1c0f0702372640866cf4deee7453e01
NatalyaLemeshevskaya <33570956+NatalyaLemeshevskaya@users.noreply.github.com>
2019-12-08 19:24:43 +0300

    Delete DataManegerWorls.swift
    delete unused file

--- EnglishTest/Models/DataManegerWorls.swift
+++
@@ -1,9 +0,0 @@
-//
-//  DataManegerWorls.swift
-//  EnglishTest
-//
-//  Created by Наташа Лемешевская on 12/7/19.
-//  Copyright © 2019 Stepan Vasilyeu. All rights reserved.
-//
-
-import Foundation
